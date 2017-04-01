78b03bba69e44e8b4c99eab1b412c8aabc8c9f61
Yuri Numerov <achi@hush.ai>
2017-04-01 14:11:09 +0200

    stuff

--- robot.ino
+++ robot.ino
@@ -17,8 +17,7 @@
 #include <Servo.h>
 #define MOTOR_IZQ 9
 #define MOTOR_DCH 10
-#define ZERO_IZQ 112     
-#define ZERO_DCH 112   
+#define ZEROS 112  
 Servo servo_izq;        // Crea los objetos para los dos servos
 Servo servo_dch;
 // Pin para el ventilador
@@ -27,44 +26,82 @@ Servo servo_dch;
 #define SUELO_IR       11
 #define MARCA_NEGRA    1
 
-        // rueda izquierda
-        //     valor > ZERO_IZQ --> adelante
-        //     valor < ZERO_IZQ --> atrás
-        // rueda derecha
-        //     valor < ZERO_DCH --> adelante
-        //     valor > ZERO_DCH --> atrás
-        
-
-
-
 void setup() {
-  // Sensor de llama
-  pinMode(LLAMA_DIG_1 , INPUT)
-  pinMode(LLAMA_ANA_1 , INPUT)
-  pinMode(LLAMA_DIG_2 , INPUT)
-  pinMode(LLAMA_ANA_2 , INPUT)
-  pinMode(LLAMA_DIG_3 , INPUT)
-  pinMode(LLAMA_ANA_3 , INPUT)
-  pinMode(LLAMA_DIG_4 , INPUT)
-  pinMode(LLAMA_ANA_4 , INPUT)
-  pinMode(LLAMA_DIG_5 , INPUT)
-  pinMode(LLAMA_ANA_5 , INPUT)
-  pinMode(US_TRIGGER  , INPUT)
-  // Motores
-  pinMode(MOTOR_IZQ , OUTPUT)
-  pinMode(MOTOR_DCH , OUTPUT)
-  pinMode(ZERO_IZQ  , OUTPUT)
-  pinMode(ZERO_DCH  , OUTPUT)
-  // Pin para el ventilador
-  pinMode(EXTINTOR    , OUTPUT)
-  // Definiciones para el sensor IR de suelo
-  pinMode(SUELO_IR    , INPUT)
-  pinMode(MARCA_NEGRA , INPUT)
 
+  
+  pinMode(LLAMA_DIG_1 , INPUT);
+  pinMode(LLAMA_ANA_1 , INPUT);
+  pinMode(LLAMA_DIG_2 , INPUT);
+  pinMode(LLAMA_ANA_2 , INPUT);
+  pinMode(LLAMA_DIG_3 , INPUT);
+  pinMode(LLAMA_ANA_3 , INPUT);
+  pinMode(LLAMA_DIG_4 , INPUT);
+  pinMode(LLAMA_ANA_4 , INPUT);
+  pinMode(LLAMA_DIG_5 , INPUT);
+  pinMode(LLAMA_ANA_5 , INPUT);
+  pinMode(US_TRIGGER  , INPUT);
+
+  servo_izq.attach(MOTOR_IZQ);
+  servo_dch.attach(MOTOR_DCH);
+  // Pin para el ventilador
+  pinMode(EXTINTOR    , OUTPUT);
+  // Definiciones para el sensor IR de suelo
+  pinMode(SUELO_IR    , INPUT);
+}
+
+int detectarlinea(){
+
+  if (digitalRead(SUELO_IR)==MARCA_NEGRA){
+    return 1;
+  }
+  return 0;
+}
+
+int detectarvela(){
+
+  if (digitalRaed(LLAMA_DIG_1)==1){return 1;}
+  if (digitalRaed(LLAMA_DIG_2)==1){return 2;}
+  if (digitalRaed(LLAMA_DIG_3)==1){return 3;}
+  if (digitalRaed(LLAMA_DIG_4)==1){return 4;}
+  if (digitalRaed(LLAMA_DIG_5)==1){return 5;}
+  return 0;
+}
+
+// rueda izquierda
+//     valor > ZERO_IZQ --> adelante
+//     valor < ZERO_IZQ --> atrás
+// rueda derecha
+//     valor < ZERO_DCH --> adelante
+//     valor > ZERO_DCH --> atrás
+void mover(dir){
+
+  if (dir=="delante"){
+    servo_dch.write(0);
+    servo_izq.write(255);
+  }
+  if (dir=="detras"){
+    servo_dch.write(255);
+    servo_izq.write(0);
+  }
+  if (dir=="izquierda"){
+    servo_dch.write(0);
+    servo_izq.write(0);
+  }
+  if (dir=="derecha"){
+    servo_dch.write(255);
+    servo_izq.write(255);
+  }
+  if (dir=="parar"){
+    servo_dch.write(ZEROS);
+    servo_izq.write(ZEROS);
+  }
 }
 
 void loop() {
 
+
 }
+
+
 
 
